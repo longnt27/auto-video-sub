7cb24cf3acfc388929633b738db0e3e1f52d9ba3
longnt27 <long.nt270504@gmail.com>
2026-09-07 14:43:43 +0700

    feat(ocr): export RapidOCR provider

--- packages/providers/src/auto_video_sub_providers/__init__.py
+++ packages/providers/src/auto_video_sub_providers/__init__.py
@@ -1,7 +1,13 @@
 """External provider adapters."""
 
-from auto_video_sub_application.ports import MediaProcessError
+from auto_video_sub_application.ports import MediaProcessError, OcrProviderError
 
 from auto_video_sub_providers.media import FFmpegMediaProcessor
+from auto_video_sub_providers.ocr import RapidOcrProvider
 
-__all__ = ["FFmpegMediaProcessor", "MediaProcessError"]
+__all__ = [
+    "FFmpegMediaProcessor",
+    "MediaProcessError",
+    "OcrProviderError",
+    "RapidOcrProvider",
+]
