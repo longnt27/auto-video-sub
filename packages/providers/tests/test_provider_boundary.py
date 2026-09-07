6f170f6e21608540ae3c3e378f2c7415afe89172
longnt27 <long.nt270504@gmail.com>
2026-09-07 19:06:22 +0700

    test(providers): include approved speech adapters

--- packages/providers/tests/test_provider_boundary.py
+++ packages/providers/tests/test_provider_boundary.py
@@ -1,12 +1,17 @@
 import auto_video_sub_providers
 
 
-def test_provider_package_exports_approved_phase4_adapters() -> None:
+def test_provider_package_exports_approved_phase5_adapters() -> None:
     assert auto_video_sub_providers.__all__ == [
         "FFmpegMediaProcessor",
+        "FFmpegSpeechAudioProcessor",
+        "LlamaCppRewriteProvider",
         "MediaProcessError",
         "OcrProviderError",
         "OpenAIResponsesTranslationProvider",
         "RapidOcrProvider",
+        "RewriteProviderError",
         "TranslationProviderError",
+        "TtsProviderError",
+        "VieNeuTtsProvider",
     ]
